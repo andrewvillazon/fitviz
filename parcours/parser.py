88c8e2eb140c0dd1113856e37fcf2643f51c323d
andrew <andrew@andrew.com>
2019-07-26 14:02:00 +1200

    Add docstring for FitMsg class

--- parcours/parser.py
+++ parcours/parser.py
@@ -1,6 +1,9 @@
 '''Module for parsing fit activity messages to model classes
 '''
 
+
 class FitMsg:
+    '''Container for a fit message. Attributes will be dynamically assigned.'''
+
     def __init__(self, msg_name):
         self.msg_name = msg_name
