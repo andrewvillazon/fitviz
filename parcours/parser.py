4ccac3061045fadf56cdf4c564e4187f5932cee3
andrew <andrew@andrew.com>
2019-07-26 20:34:42 +1200

    Rename FitMsg and expand functionality

--- parcours/parser.py
+++ parcours/parser.py
@@ -1,9 +1,25 @@
 '''Module for parsing fit activity messages to model classes
 '''
 
+from . import models
 
-class FitMsg:
-    '''Container for a fit message. Attributes dynamically assigned.'''
 
-    def __init__(self, msg_name):
-        self.msg_name = msg_name
+class FitMessageData:
+    '''A wrapper around a fit message field data.
+    
+    Takes a fit message and sets the message's field data to the objects
+    attributes. Makes the message easier to use by providing direct
+    access to the field names and data.
+    '''
+
+    def __init__(self, message):
+        self.message_name = message.name
+        self.__dict__.update(self.extract_field_data(message.fields))
+
+    def extract_field_data(self,fields):
+        field_data = {}
+        
+        for field in fields:
+            field_data[field.name]=field.value
+
+        return field_data
