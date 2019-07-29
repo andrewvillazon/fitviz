23cba22e5b97d27b8ff55d64c0694e40cfb81e74
andrew <andrew@andrew.com>
2019-07-29 21:28:51 +1200

    Add record mapper

--- parcours/parser.py
+++ parcours/parser.py
@@ -1,7 +1,12 @@
 '''Module for parsing fit activity messages to model classes
 '''
 
-from .models import Lap
+from pyproj import Proj, transform
+
+from .models import Lap, Record
+
+
+DEGREES_MULTIPLIER = 180 / 2 ** 31
 
 
 class MessageWrapper:
@@ -41,6 +46,45 @@ class LapMapper:
         }
 
         self.dest_model.__dict__.update(mapping)
+
+    def mapped_model(self):
+        return self.dest_model
+
+
+class RecordMapper:
+
+    def __init__(self, source_msg):
+        self.source_msg = source_msg
+        self.dest_model = Record()
+
+        self.apply_mapping()
+
+    def apply_mapping(self):
+        mapping = {
+            "recorded_dtm": self.source_msg.timestamp,
+            "heart_rate":self.source_msg.heart_rate,
+            "power": self.source_msg.power,
+            "cadence": self.source_msg.cadence,
+            "speed": self.source_msg.speed,
+            "distance": self.source_msg.distance,
+            "altitude": self.source_msg.altitude,
+            "latitude": self.semicircles_to_degrees(self.source_msg.position_lat),
+            "longitude": self.semicircles_to_degrees(self.source_msg.position_long)
+        }
+
+        mapping["longitude_mercator"], mapping["latitude_mercator"] = self.latlong_to_mercator(mapping["longitude"],mapping["latitude"])
+
+        self.dest_model.__dict__.update(mapping)
+
+    def semicircles_to_degrees(self,semicircle):
+        degrees = semicircle * (DEGREES_MULTIPLIER)
+        return round(degrees,6)
+
+    def latlong_to_mercator(self,long,lat):
+        latlong = Proj(init='epsg:4326')
+        mercator = Proj(init='epsg:3857')
+
+        return transform(latlong,mercator,long,lat)
 
     def mapped_model(self):
         return self.dest_model
