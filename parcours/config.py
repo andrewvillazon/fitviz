9523a74bc89a39ac13ddce791cec903b8171bdb2
andrew <andrew@andrew.com>
2019-08-12 21:14:27 +1200

    WIP config module

--- parcours/config.py
+++ parcours/config.py
@@ -1,4 +1,20 @@
+import configparser
 import os
 
-if __name__ == "__main__":
-    print(os.getenv("PARCOURS_CONF"))
+CONFIG_DIR = os.getenv('PARCOURS_CONF')
+
+class Configuration:
+    def __init__(self, config_parser):
+        parser = config_parser
+    
+    def __getattr__(self,attr):
+        return self.parser.get(attr, None)
+
+
+cp = configparser.SafeConfigParser()
+cp.read(CONFIG_DIR)
+config = Configuration(config_parser)
+
+# TODO: Add nested attribute access to support config.section.value
+# TODO: http://code.activestate.com/recipes/577346-getattr-with-arbitrary-depth/
+# TODO: http://code.activestate.com/recipes/426406-an-easy-to-use-configuration-reader/
