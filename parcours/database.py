ee64135375121fd07dc11298e9f9056943b3d77f
andrew <andrew@andrew.com>
2019-08-20 20:16:28 +1200

    Add database creation

--- parcours/database.py
+++ parcours/database.py
@@ -1,3 +1,25 @@
-"""This module provides functionality for loading activity files to 
-the database.
 """
+parcours.database
+~~~~~~~~~~~~~~~
+
+This module contains functionality for setting up and loading the
+parcours database.
+"""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .config import config
+from .models import Base
+
+
+def load_db():
+    """
+    Sets up and populates the parcours database. Will create db and
+    schema if does not exist. Reads activity files from the activity
+    directory and loads them to the db.
+    """
+
+    engine = create_engine("sqlite:///{}".format(config["db_uri"]), echo=True)
+    Base.metadata.create_all(engine)
