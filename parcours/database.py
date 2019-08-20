a5b3fa655d5f3cd29f65529c358682e504d3caf3
andrew <andrew@andrew.com>
2019-08-20 20:26:42 +1200

    Add method to return activty directory contents

--- parcours/database.py
+++ parcours/database.py
@@ -6,12 +6,26 @@ This module contains functionality for setting up and loading the
 parcours database.
 """
 
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from .config import config
 from .models import Base
+
+
+def activities_in_dir():
+    """
+    Returns a list of the activity files in the activity directory.
+    """
+    file_names = []
+
+    for file_name in os.listdir(config["activity_dir"]):
+        if file_name.endswith(".fit"):
+            file_names.append(file_name)
+
+    return file_names
 
 
 def load_db():
