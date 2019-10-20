478d22c0ec6aaa29bbe5618739c6c617db103c20
andrew <andrew@andrew.com>
2019-10-20 21:26:33 +1300

    Add database load to server start lifecycle hook

--- server_lifecycle.py
+++ server_lifecycle.py
@@ -5,8 +5,9 @@ server_lifecycle
 Bokeh Server Lifecycle Hooks. Code here is designed to be exectuted at
 certain times in the server session or lifetime.
 """
+from parcours import database
 
 
 def on_server_loaded(server_context):
     """Function called when bokeh server first starts."""
-    print("Server started. on_server_loaded called. Preparing database.")
+    database.load_db()
